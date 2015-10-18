56260fa7f77fc8dbde14de5e7ebe64f0cad22f2f
Ellington Kirby <ellingtonkirby31@gmail.com>
2015-10-18 19:45:06 -0400

    added methods supporting usage of skip_list, insert and search. Declared Node structs and data members used to support skip list

--- Map.hpp
+++ Map.hpp
@@ -1,7 +1,31 @@
 #include <iostream>
 #include <utility>
+#include <random>
 
+namespace cs540{
 template<typename Key_T, typename Mapped_T> class Map{
+	private:
+		int max_height;
+		int rheight();
+		struct Node{
+			int height;
+			Key_T key;
+			Mapped_T value;
+			Node **next;
+			Node::Node(Key_T keyed, Mapped_T valued, int applied_height = rheight()){
+				key = keyed;
+				value = valued;
+				height = applied_height;
+				Node **next = new Node[max_height];
+			};
+			Node::~Node(){
+				delete[] next;
+			}
+		};
+		Node * head;
+		int curr_height;
+		std::pair<Key_T, Mapped_T> search(Key_T key);	
+		int skip_list_insert(Key_T key, Mapped_T val);
 	public:
 		//Member Type
 		typedef std::pair<const Key_T, Mapped_T> ValueType;
@@ -86,20 +110,91 @@ template<typename Key_T, typename Mapped_T> class Map{
 
 
 };
-
+}
 //Comparison
 bool operator==(const Map &, const Map &);
 bool operator!=(const Map &, const Map &);
 bool operator<(const Map &, const Map &);
 //Implement all comparisons between iterators
 
+Map::Map(){
+	//Need to construct sentinel Nodes head and tail. All next in tail will point to null, head will point to tail at first
+	int max_height = 16; //TODO find out what to do with max height
+	Node * head = new Node(NULL, NULL, max_height);
+	Node * tail = new Node(NULL, NULL, max_height);
+	for(int i = 0; i < max_height; i++){
+		head->next[i] = tail;
+		tail->next[i] = NULL;
+	}
 
+	//What's next?
+}
+//Function that determines the random height of a column. All credit to Eternally Confuzzled at eternallyconfuzzled.com/tuts/datastructures/jsw_tut_skip.aspx
+int Map::rheight(){
+	static int bits = 0;
+	static int reset = 0;
+	int h, found = 0;
+	std::random_device rd;
+	for(h = 0; !found; h++){
+		if(reset == 0){
+			bits = rd();
+			reset = sizeof(int) * CHAR_BIT;
+		}
+		found  = bits & 1;
+		bits = bits >> 1;
+		--reset;
+	}
 
+	if(h >= max_height){
+		h = max_height - 1;
+	}
+	return h;
+}
 
+std::pair<Key_T, Mapped_T> Map::search(Key_T key){
+	Node * it = this->head;
+	int i;
 
+	for(i = this->curr_height - 1; i >= 0; i--){
+		while(it->next[i] != NULL && next[i]->key < key){
+			it = it->next[i];
+		}
+	}
 
+	return it->next[0] ? pair<Key_T, Mapped_T>(it->next[0]->key, it->next[0]->value) : NULL;
+}
 
+int Map::skip_list_insert(Key_T key, Mapped_T value){
+	Node *it = this->head;
+	Node *item  = new Node(key, value);
+	Node **fix = new Node[max_height];
+	int i;
 
+	for(i = curr_height - 1; i >= 0; i--){
+		while(it->next[i] != NULL && it->next[i]->key < key){
+			it = it->next[i];
+		}
+		fix[i] = it;
+	}
 
+	if(it->next[0] != NULL && it->next[0]->key == key){
+		return 0;
+	}
+	else{
+		int h;
+		while(item->height > curr_height){
+			fix[curr_height++] = this->head;
+		}
+		
+		h = item->height;
+		while(--h >= 0){
+			item->next[h] = fix[h]->next[h];
+			fix[h]->next[h] = item;
+		}
+	}
+	delete[] fix;
+	return 1;
+}
+		
 
 
